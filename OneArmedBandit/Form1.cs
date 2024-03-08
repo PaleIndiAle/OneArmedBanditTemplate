a560769c887566bf3a41fd97c993455a43176af5
Kevnel YT <kevichen510@gmail.com>
2024-03-07 21:36:55 -0500

    WINNER!!!

--- OneArmedBandit/Form1.cs
+++ OneArmedBandit/Form1.cs
@@ -12,9 +12,10 @@ namespace OneArmedBandit
     public partial class Form1 : Form
     {
         // random number generator
+        Random randgen = new Random();
 
         // int value for score initialized to 10
-
+        int value = 10;
 
         public Form1()
         {
@@ -24,18 +25,54 @@ namespace OneArmedBandit
         private void spinButton_Click(object sender, EventArgs e) 
         {
             // get random values for each reel (store each in separate int variable)
-
+            int slot1 = randgen.Next(1, 4);
+            int slot2 = randgen.Next(1, 4);
+            int slot3 = randgen.Next(1, 4);
 
             // check value of reel 1 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
-
+            switch (slot1)
+            {
+                case 1:
+                    pictureBox1.BackgroundImage = OneArmedBandit.Properties.Resources._7_100x125;
+                    break;
+                case 2:
+                    pictureBox1.BackgroundImage = OneArmedBandit.Properties.Resources.cherry_100x125;
+                    break;
+                case 3:
+                    pictureBox1.BackgroundImage = OneArmedBandit.Properties.Resources.diamond_100x125;
+                    break;
+            }
 
             // check value of reel 2 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
+            switch (slot2)
+            {
+                case 1:
+                    pictureBox2.BackgroundImage = OneArmedBandit.Properties.Resources._7_100x125;
+                    break;
+                case 2:
+                    pictureBox2.BackgroundImage = OneArmedBandit.Properties.Resources.cherry_100x125;
+                    break;
+                case 3:
+                    pictureBox2.BackgroundImage = OneArmedBandit.Properties.Resources.diamond_100x125;
+                    break;
+            }
 
             // check value of reel 3 with a switch statement to set appropriate image to
             // BackgroundImage property of pictureBox. Get images from Properties.Resources
-
+            switch (slot3)
+            {
+                case 1:
+                    pictureBox3.BackgroundImage = OneArmedBandit.Properties.Resources._7_100x125;
+                    break;
+                case 2:
+                    pictureBox3.BackgroundImage = OneArmedBandit.Properties.Resources.cherry_100x125;
+                    break;
+                case 3:
+                    pictureBox3.BackgroundImage = OneArmedBandit.Properties.Resources.diamond_100x125;
+                    break;
+            }
 
             /// STOP HERE ----------------------------------------------------------
             /// Test to make sure that your program will display random
@@ -45,13 +82,28 @@ namespace OneArmedBandit
 
             // Use compound if statement to check if all reels are equal. 
             // If yes show "winner" statement and add 3 to score.
-            // If no show "play again" statement and subtract 1 from score.         
+            // If no show "play again" statement and subtract 1 from score.
+            if (slot1 == slot2 && slot1 == slot3)
+            {
+                outputLabel.Text = "Winner!!!";
+                value = value + 3;
+            }
+            else
+            {
+                outputLabel.Text = "Loser!!!";
+                value = value - 1;
+            }
 
 
             // if score has reached 0 display "lose" message and set button enabled property to false
-
+            if (value == 0)
+            {
+                outputLabel.Text = "Lose";
+                spinButton.Enabled = false;
+            }
 
             // display updated score
+            scoreDisplay.Text = $"{value}";
 
         }
     }
